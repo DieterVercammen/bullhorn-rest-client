d36b31173c2126e7b31ba56635db611c45d7d5ad
Dieter Vercammen <dieter@lunargravity.be>
2022-09-06 16:06:30 +0200

    Remove faulty session check for bullhorn auth

--- src/Client.php
+++ src/Client.php
@@ -97,10 +97,10 @@ class Client
         // adds session initialization if requests made when no session is active
         $this->httpHandlerStack->push(function (callable $handler) {
             return function (RequestInterface $request, array $options) use ($handler) {
-                if ($this->sessionIsValid()) {
-                    return $handler($request, $options);
-                }
-                else {
+//                if ($this->sessionIsValid()) {
+//                    return $handler($request, $options);
+//                }
+//                else {
                     $this->refreshOrInitiateSession();
                     $requestPath = str_replace($this->authClient->getRestUrl(), '', $request->getUri());
                     $refreshedRequest = $request
@@ -108,7 +108,7 @@ class Client
                         ->withUri(new Uri($this->authClient->getRestUrl() . $requestPath))
                     ;
                     return $handler($refreshedRequest, $options);
-                }
+//                }
             };
         });
 
